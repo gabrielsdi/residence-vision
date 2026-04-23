867a3d7175b48962e231b25869b8fb91d1d5b970
gabrielsdi <gabrielsdi18@gmail.com>
2026-04-23 11:51:54 -0300

    fix error pages styles and change primary color

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function ErrorPage({
   errorStatus,
   handleClick,
@@ -7,13 +10,29 @@ export default function ErrorPage({
   errorStatus: string;
   handleClick: () => void;
 }) {
+  const ERROR_PAGE_MESSAGE = "Oops something went wrong";
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
-      <h1 className="text-2xl font-bold mb-4">{errorStatus}</h1>
-      <p className="text-red-500">Ops houses cant be loaded</p>
-      <button onClick={handleClick} className="mt-4 underline text-sm">
-        Retry
-      </button>
+    <div className="flex flex-col flex-1 items-center justify-center bg-white dark:bg-black pt-26 ">
+      <Image
+        data-testid="error-page-image"
+        src="/error-house-image.svg"
+        alt="error-page-image"
+        width={400}
+        height={80}
+        className="object-contain"
+        priority
+      />
+      <h1 className="text-primary text-3xl font-bold mb-4">
+        {errorStatus || "Error"}
+      </h1>
+      <p className="text-primary text-2xl">{ERROR_PAGE_MESSAGE}</p>
+      <Button
+        onClick={handleClick}
+        size="lg"
+        className="mt-4 cursor-pointer w-30 h-10"
+      >
+        Go Home
+      </Button>
     </div>
   );
 }
